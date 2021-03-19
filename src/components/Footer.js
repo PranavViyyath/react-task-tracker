9b130338cc5e2c54355b98ff60b5d34846975c28
Sarath VS <sarathvss65@gmail.com>
2021-03-19 18:02:59 +0530

    Update components

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copy right @2021</p>
+            <Link to='/About'>About</Link>
+        </footer>
+    )
+}
+
+export default Footer
